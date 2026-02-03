dc8427558746231b2e9ef815db1955d726e0d673
Motldsha <lmpyuuki@outlook.com>
2026-02-03 11:54:00 +0800

    update

---
+++ Assets/Scripts/Gameplay/Terrain/MobilePlatform.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobilePlatform : MonoBehaviour
+{
+    public Transform startPoint;
+    public Transform endPoint;
+    public float speed;
+    public EventCenter eventCenter;
+    public SpriteRenderer sprite;
+    public BoxCollider2D collider;
+    public ColorType colorType;
+    public float tolerance = 0.01f;
+
+    private Vector2 _target;
+    private Vector2 _start;
+    private Vector2 _end;
+    private void Awake()
+    {
+        eventCenter = EventCenter.Instance;
+        transform.position = startPoint.position;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        sprite = gameObject.GetComponent<SpriteRenderer>();
+        collider = gameObject.GetComponent<BoxCollider2D>();
+        eventCenter.AddEventListener(EventType.ColorChange, OnColorChanged);
+        _start = new Vector2(startPoint.position.x, startPoint.position.y);
+        _end = new Vector2(endPoint.position.x, endPoint.position.y);
+        _target = _end;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 pos = new Vector2(transform.position.x, transform.position.y);
+        Vector2 next = Vector2.MoveTowards(pos, _target, speed * Time.deltaTime);
+        transform.position = new Vector3(next.x, next.y, transform.position.z);
+
+        if (Vector2.Distance(next, _target) <= tolerance)
+        {
+            _target = (_target == _end) ? _start : _end;
+        }
+    }
+
+    void OnColorChanged(object info)
+    {
+        ColorType type = (ColorType)Enum.ToObject(typeof(ColorType), info);
+        ChangespriteState(type);
+    }
+
+    public virtual void ChangespriteState(ColorType type)
+    {
+        if (colorType == type && colorType != ColorType.WHITE)
+        {
+            sprite.color = new Color(sprite.color.r, sprite.color.g, sprite.color.b, 100);
+            collider.enabled = false;
+        }
+        else
+        {
+            sprite.color = new Color(sprite.color.r, sprite.color.g, sprite.color.b, 255);
+            collider.enabled = true;
+        }
+    }
+}
---
+++ Assets/Scripts/Gameplay/Terrain/TilemapBase.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Tilemaps;
+
+public class TilemapBase : MonoBehaviour
+{
+    public EventCenter eventCenter;
+
+    public Tilemap tilemap;
+    public TilemapCollider2D tilemapCollider2D;
+    public ColorType colorType;
+    private void Awake()
+    {
+        eventCenter = EventCenter.Instance;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        tilemap = gameObject.GetComponent<Tilemap>();
+        tilemapCollider2D = gameObject.GetComponent<TilemapCollider2D>();
+        eventCenter.AddEventListener(EventType.ColorChange, OnColorChanged);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void OnColorChanged(object info)
+    {
+        ColorType type = (ColorType)Enum.ToObject(typeof(ColorType), info);
+        ChangeTileMapState(type);
+    }
+
+    public virtual void ChangeTileMapState(ColorType type) 
+    {
+        if (colorType == type && colorType != ColorType.WHITE)
+        {
+            tilemap.color = new Color(tilemap.color.r, tilemap.color.g, tilemap.color.b, 100);
+            tilemapCollider2D.enabled = false;
+        }
+        else
+        {
+            tilemap.color = new Color(tilemap.color.r, tilemap.color.g, tilemap.color.b, 255);
+            tilemapCollider2D.enabled = true;
+        }
+    }
+}
